408e4d07f386d0e2dfef3953e983a354ae98fcda
Stevanus Prasetyo Soemadi <stevanus.pras@gmail.com>
2019-10-12 16:44:14 +0700

    Add new layers and wallpaper

---
+++ AutoLayoutAndConstraints/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  AutoLayoutAndConstraints
+//
+//  Created by Stevanus Prasetyo Soemadi on 12/10/19.
+//  Copyright © 2019 Stevanus Prasetyo Soemadi. All rights reserved.
+//
+
+import UIKit
+
+struct Colors {
+    static let tropicBlue = UIColor(red: 0, green: 192/255, blue: 255/255, alpha: 1)
+}
+
+
+struct Fonts {
+    static let avenirNextCondensedDemiBold = "AvenirNextCondensed-DemiBold"
+}
--- AutoLayoutAndConstraints/ViewController.swift
+++ AutoLayoutAndConstraints/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let backgroundImageView = UIImageView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+//        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIInpu,tViewController.dismissKeyboard))
+
+        setBackground()
+//        view.addGestureRecognizer(tap)
     }
-
-
+    
+    func setBackground() {
+        view.addSubview(backgroundImageView)
+        backgroundImageView.translatesAutoresizingMaskIntoConstraints = false
+        backgroundImageView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        backgroundImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        backgroundImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        backgroundImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        
+        backgroundImageView.image = UIImage(named: "bgView")
+        view.sendSubviewToBack(backgroundImageView)
+    }
+    
+//    @objc func dismissKeyboard() {
+//        view.endEditing(true)
+//    }
 }
-
